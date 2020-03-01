83aa3426506b4d67689a33081b8cbcb2f0f60b54
Lok Chan <loklokj@gmail.com>
2020-03-01 14:43:45 -0500

    wording

--- pages/mt2.js
+++ pages/mt2.js
@@ -45,7 +45,7 @@ const featureDict = {
   previouslyReceived: "Previously Received an Organ Transplant",
   pastContribution: "Past contributions to hospital",
   mentalHealth: "current mental health",
-  postHabit: "Post-Diagnosis Drug/Alcohol Use/Abuse"
+  postHabit: "Post-Diagnosis Drug/Alcohol Use"
 };
 
 const targetEdDistractionFeature = [
